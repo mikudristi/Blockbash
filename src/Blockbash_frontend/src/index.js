50c547f99eb7a070a2536c6cf25f4565e0ff70a7
Dristi <115622719+mikudristi@users.noreply.github.com>
2023-11-22 13:21:17 +0000

    done

---
+++ src/Blockbash_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { Blockbash_backend } from "../../declarations/Blockbash_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Blockbash_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
